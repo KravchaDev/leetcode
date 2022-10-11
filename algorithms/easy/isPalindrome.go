5a4dad91db785bc2cc19e0387bafa3eb42219883
Данила Кравченко <88050202+KravchaDev@users.noreply.github.com>
2022-10-11 11:07:18 +0800

    Update isPalindrome.go

--- algorithms/easy/isPalindrome.go
+++ algorithms/easy/isPalindrome.go
@@ -1,32 +1,13 @@
-package	main
-
 func isPalindrome(x int) bool {
-	// записываем изначально переданное число в другую переменную, для дальнейших действий с исходным X
-    initial := x
-
-	// ноль всегда палиндром
-    if x == 0 {
-        return true
-    }
-    
-	// числа меньше нуля всегда не палиндром
-    if x < 0 {
-        return false
-    }
-	
-    // создаем переменную для конечного числа
-	finite := 0
-	for ; x != 0 ; x/=10 {
-		last := x % 10
-		finite = finite*10 + last
+	if x < 0 {
+		return false
 	}
-	return(initial==finite)
-}
+	r := 0
+	for z := x; z > 0; z /= 10 {
+		 tmp := z % 10
+		 r *= 10
+		 r += tmp
 
-func main(){
-	println("7554 is palindrome?  - ", isPalindrome(7554)) // false
-	println("0    is palindrome?  - ", isPalindrome(0))	   // true
-	println("-121 is palindrome?  - ", isPalindrome(-121)) // false
-	println("121 is palindrome?   - ", isPalindrome(121))  // true
-	println("4224 is palindrome?  - ", isPalindrome(4224)) // true
+	}
+	return r == x
 }
