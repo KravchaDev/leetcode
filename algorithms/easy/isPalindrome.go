312e02408e228e89fa9480c9f2b0b61730794f31
bot-kravcha <109323405+bot-kravcha@users.noreply.github.com>
2022-10-11 11:03:34 +0800

    Update isPalindrome.go

--- algorithms/easy/isPalindrome.go
+++ algorithms/easy/isPalindrome.go
@@ -1,32 +1,17 @@
-package	main
-
 func isPalindrome(x int) bool {
-	// записываем изначально переданное число в другую переменную, для дальнейших действий с исходным X
-    initial := x
-
-	// ноль всегда палиндром
-    if x == 0 {
-        return true
-    }
-    
-	// числа меньше нуля всегда не палиндром
-    if x < 0 {
-        return false
-    }
-	
-    // создаем переменную для конечного числа
-	finite := 0
-	for ; x != 0 ; x/=10 {
-		last := x % 10
-		finite = finite*10 + last
+	if math.Signbit(float64(x)) {
+		return false
 	}
-	return(initial==finite)
-}
-
-func main(){
-	println("7554 is palindrome?  - ", isPalindrome(7554)) // false
-	println("0    is palindrome?  - ", isPalindrome(0))	   // true
-	println("-121 is palindrome?  - ", isPalindrome(-121)) // false
-	println("121 is palindrome?   - ", isPalindrome(121))  // true
-	println("4224 is palindrome?  - ", isPalindrome(4224)) // true
-}
+	rev := 0
+	remainder := 0
+	argz := x
+	for argz > 0 {
+		rev *= 10
+		remainder = argz % 10
+		rev += remainder
+		argz /= 10
+	}
+	if rev == x {
+		return true
+	}
+	return false
