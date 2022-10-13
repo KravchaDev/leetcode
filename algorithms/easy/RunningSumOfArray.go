9bd78194c7c7c30148ee483519d5650bb7e09ae7
Danila Kravchenko <coderkravcha@gmail.com>
2022-10-13 15:01:51 +0800

    3 day: adding solution to task Running Sum of 1d array

---
+++ algorithms/easy/RunningSumOfArray.go
@@ -0,0 +1,20 @@
+package main
+
+func outArray(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		println(nums[i])
+	}
+}
+
+func runningSum(nums []int) []int {
+    for i := 1; i < len(nums); i++ {
+		nums[i] = nums[i] + nums[i-1]
+	}
+	return nums
+}
+
+func main() {
+	nums := [8]int{1, 2, 3, 4, 5, 6, 7, 8}
+	runningSum(nums[:])
+	outArray(nums[:])
+}
