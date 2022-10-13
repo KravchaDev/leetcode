daa133d067f2991ad6ca99bb3068295f7114107e
Danila Kravchenko <coderkravcha@gmail.com>
2022-10-13 09:24:36 +0800

    3 day: optimization swap variables

--- algorithms/easy/sortThePeople.go
+++ algorithms/easy/sortThePeople.go
@@ -5,13 +5,8 @@ func sortPeople(names []string, heights []int) []string {
 	for i := 0; i < len(names); i++ {
 		for j := 0; j < len(names)-1-i; j++ {
 			if heights[j] < heights[j+1] {
-				declaratedName := names[j]
-				names[j] = names[j+1]
-				names[j+1] = declaratedName
-
-				declaratedHeight := heights[j]
-				heights[j] = heights[j+1]
-				heights[j+1] = declaratedHeight
+				names[j], names[j+1] = names[j+1], names[j]
+				heights[j], heights[j+1] = heights[j+1], heights[j]
 			}
 		}
 	}
@@ -20,9 +15,14 @@ func sortPeople(names []string, heights []int) []string {
 
 func main() {
 	names := [8]string{"Драган", "Милица", "Никола", "Петар ", "Елена", "Марко", "Лука ", "Джэгода"}
+	println("Неотсортированный массив:")
+	for i := 0; i < len(names); i++ {
+		println(names[i])
+	}
 	heights := [8]int{155, 168, 160, 195, 178, 153, 161, 193}
 	sortPeople(names[:], heights[:])
 
+	println("\nОтсортированный массив:")
 	for i := 0; i < len(names); i++ {
 		println(names[i])
 	}
