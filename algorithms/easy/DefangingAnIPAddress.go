369b00070af5cd168da846272789b57759a64721
Danila Kravchenko <coderkravcha@gmail.com>
2022-10-14 11:09:19 +0800

    4 day: adding solution with not use STRINGS and FMT modules

--- algorithms/easy/DefangingAnIPAddress.go
+++ algorithms/easy/DefangingAnIPAddress.go
@@ -1,11 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+func defangIPaddr(address string) string {
+	var output string
+	for i := 0; i < len(address); i++ {
+		if string(address[i]) == "." {
+			output += "[.]"
+		} else {
+			output += string(address[i])
+		}
+	}
+	println(output)
+	return output
+}
 
 func main() {
-	refString := "255.100.50.0"
-	fmt.Println(strings.Replace(refString, ".", "[.]", -1))
+	defangIPaddr("255.100.50.0")
+	defangIPaddr("95.248.195.127")
+	defangIPaddr("39.27.141.110")
 }
