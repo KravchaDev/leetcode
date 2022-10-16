e3964b1828ac8e96aadb20950ef8409392f31d3c
Danila Kravchenko <coderkravcha@gmail.com>
2022-10-16 23:14:20 +0800

    auto formating code & structuring files by folders

--- algorithms/easy/RunningSumOfArray.go
+++ algorithms/easy/RunningSumOfArray.go
@@ -7,7 +7,7 @@ func outArray(nums []int) {
 }
 
 func runningSum(nums []int) []int {
-    for i := 1; i < len(nums); i++ {
+	for i := 1; i < len(nums); i++ {
 		nums[i] = nums[i] + nums[i-1]
 	}
 	return nums
--- base go syntax/addTwoIntenger.go
+++ base go syntax/addTwoIntenger.go
@@ -1,10 +1,10 @@
 package main
 
 func sum(num1 int, num2 int) int {
-    return num1 + num2
+	return num1 + num2
 }
 
-func main(){
+func main() {
 	println(sum(54, -6))
 	println(sum(-51, 24))
 }
